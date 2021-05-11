97ca26084a0ccdb99b71ae581b34bfe17ba72481
Marcus Rangel <marcus.vbrangel@gmail.com>
2021-05-11 01:20:00 -0300

    feat: added feature modal transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,23 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+
+    onOpenNewTransactionModal: () => void;
+
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
 
     return (
 
-        <Container>
+        <Container> 
 
             <Content>
                 
                 <img src={logoImg} alt="dtmoney" />
 
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova Transação
                 </button>
 
